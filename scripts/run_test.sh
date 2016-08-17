1dca73ef0e901a72d7ca9d68fc408a12c961a6ce
Aviv Cukierman <acukierm@slac.stanford.edu>
2016-08-17 07:10:59 -0700

    First commit. Reads truth information.

---
+++ scripts/run_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python GGYYAnalysis/scripts/Run.py \
+    --submitDir "Test" \
+    -w \
+    --inputFiles "/nfs/slac/g/atlas/u01/users/acukierm/VBF_Analysis/Test_Sample" \
+    --nevents 10 \
+    --mode "class" \
+    --driver "direct"
+exit 0
+
+#--inputFiles "/atlas/dq2/user/bnachman/" \
+    #--inputFiles "/nfs/slac/g/atlas/u01/users/acukierm/VBF/mc_sample/" \
