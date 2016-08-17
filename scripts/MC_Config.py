d483078bc9a3b0a81175e51f216e13e33b464e3d
Aviv Cukierman <acukierm@slac.stanford.edu>
2016-08-17 13:28:00 -0700

    Added jet calibration and jet selection

--- scripts/MC_Config.py
+++ scripts/MC_Config.py
@@ -8,6 +8,9 @@ sys.path.insert(0, os.environ['ROOTCOREBIN']+"/user_scripts/GGYYAnalysis/")
 
 c = xAH_config()
 
+#
+# Triggers
+#
 triggersList = [
   'L1_4J20',
   'L1_J100',
@@ -61,8 +64,9 @@ triggersList = [
   'HLT_mu26_imedium'
 ]
 
-
-
+#
+# Event Selection
+#
 triggers = ",".join(triggersList)
 c.setalg("BasicEventSelection", {
   "m_name"                  : "basicEventSel",
@@ -85,8 +89,70 @@ c.setalg("BasicEventSelection", {
   "m_storeTrigKeys"         : True,
 } )
 
+#
+# Uncalibrated Jets
+#
 c.setalg("JetHistsAlgo", {
   "m_name":"Jets_Uncalib/",
   "m_inContainerName":"AntiKt4LCTopoJets", 
-  "m_detailStr" : "kinematic energy NLeading4"
+  "m_detailStr" : "kinematic energy"
+} )
+
+#
+#  Jet Calibration
+#
+c.setalg("JetCalibrator", {
+  "m_name"                   : "AntiKt4TopoEM", 
+  "m_inContainerName"        : "AntiKt4EMTopoJets",
+  "m_outContainerName"       : "AntiKt4EMTopoJets_Calib", 
+  "m_sort"                   : True,
+  "m_jetAlgo"                : "AntiKt4EMTopo",
+  "m_outputAlgo"             : "AntiKt4EMTopoJets_Calib_Algo",
+  "m_calibSequence"          : "JetArea_Residual_Origin_EtaJES_GSC",
+  "m_calibConfigFullSim"     : "JES_MC15cRecommendation_May2016.config",
+  "m_calibConfigData"        : "JES_MC15cRecommendation_May2016.config",
+  "m_calibConfigAFII"        : "JES_MC15Prerecommendation_AFII_June2015.config",
+  "m_jetCleanCutLevel"       : "LooseBad",
+  "m_JESUncertConfig"        : "$ROOTCOREBIN/data/JetUncertainties/JES_2015/ICHEP2016/JES2015_AllNuisanceParameters.config",
+  "m_JESUncertMCType"        : "MC15",
+  "m_saveAllCleanDecisions"  : True,                         
+  "m_setAFII"                : False,
+  "m_JERUncertConfig"        : "JetResolution/Prerec2015_xCalib_2012JER_ReducedTo9NP_Plots_v2.root",
+  "m_JERApplyNominal"        : False,
+  "m_redoJVT"                : True,
+  "m_systName"               : "All",
+  "m_systVal"                : 1
+} )
+
+#
+# Calibrated Jets
+#
+c.setalg("JetHistsAlgo", {
+  "m_name":"Jets_Calib/",
+  "m_inContainerName":"AntiKt4EMTopoJets_Calib", 
+  "m_detailStr" : "kinematic energy NLeading1"
+} )
+
+#
+#  Jet Selection
+#
+c.setalg("JetSelector", {
+  "m_name"                    :  "preSelJetsEMTopoJets",
+  "m_inContainerName"         :  "AntiKt4EMTopoJets_Calib",
+  "m_inputAlgo"               :  "AntiKt4EMTopoJets_Calib_Algo",
+  "m_outContainerName"        :  "AntiKt4EMTopoJets_Calib_preSel",
+  "m_outputAlgo"              :  "AntiKt4EMTopoJets_Calib_preSel_Algo",
+  "m_decorateSelectedObjects" :  False, 
+  "m_createSelectedContainer" :  True, 
+  "m_cleanJets"               :  True, 
+  "m_cleanEvtLeadJets"        :  99,
+  "m_pT_min"                  :  20e3,
+  "m_eta_max"                 :  4.9,
+  "m_useCutFlow"              :  True,
+  "m_doJVT"                   :  True,
+  "m_pt_max_JVT"              :  60e3,
+  "m_eta_max_JVT"             :  2.4,
+  "m_WorkingPointJVT"         :  "Medium",
+  "m_jetScaleType"            :  "JetConstitScaleMomentum",
+  "m_debug"                   :  False
 } )
